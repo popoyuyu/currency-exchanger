b7983c47ec5581adf453658b819753ec149e98bc
Jeff Lai <pjefflai@gmail.com>
2021-11-19 11:07:27 -0800

    WIP: reading up on promises async/await

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { ClassExample1, ClassExample2 } from './project';
+import ExchangeRate from './exchange.js';
+
+function getElement(response, firstCurrency, secondCurrency) {
+
+}
+
+async function makeApiCall(firstCurrency, secondCurrency) {
+  const response = await ExchangeRate.getConversion(firstCurrency, secondCurrency);
+  getElements(response, firstCurrency, secondCurrency);
+}
