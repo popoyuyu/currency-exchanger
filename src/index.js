c0d63a8a8b6729ed8749438dc4a6c3b11f39fa3f
Jeff Lai <pjefflai@gmail.com>
2021-11-19 13:53:31 -0800

    WIP: submit button not working

--- src/index.js
+++ src/index.js
@@ -4,14 +4,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeRate from './exchange.js';
 
-function getElement(response, firstCurrency, secondCurrency) {
+function getElement(response, firstCurrency, secondCurrency, usdAmount) {
   if (response.result === "success") {
-
+    console.log(response);
+    $("#currencyOutput").text(`&dollar;${usdAmount} ${firstCurrency} = ${response.conversion_rate * secondCurrency}`);
+  } else {
+    $("#error").text(`Sorry, there wasn an error: ${response}`);
   }
-
 }
 
 async function makeApiCall(firstCurrency, secondCurrency) {
   const response = await ExchangeRate.getConversion(firstCurrency, secondCurrency);
-  getElements(response, firstCurrency, secondCurrency);
+  getElement(response, firstCurrency, secondCurrency);
 }
+
+$(document).ready(function () {
+  $("#exchanger").on("click", function (e) {
+    e.preventDefault();
+    console.log("hello");
+    let firstCurrency = $("#USD").val();
+    let secondCurrency = $("#foreignCurrency option:selected").val();
+    makeApiCall(firstCurrency, secondCurrency);
+  });
+});
+
+// $("#exchanger").click(function (e) {
+//   e.preventDefault();
+//   console.log("hello");
+//   let firstCurrency = $("#USD").val();
+//   let secondCurrency = $("#foreignCurrency option:selected").val();
+//   makeApiCall(firstCurrency, secondCurrency);
+// });
