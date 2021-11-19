882741f0ac519013866b1e85368cc4aa81f8b098
Jeff Lai <pjefflai@gmail.com>
2021-11-19 10:46:58 -0800

    html setup

---
+++ src/exchange.js
@@ -0,0 +1,14 @@
+export default class ExchangeRate {
+  static async getRate(currency) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}`)
+      .then(function (response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function (error) {
+        return error;
+      })
+  }
+}
