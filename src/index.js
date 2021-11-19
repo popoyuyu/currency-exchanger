49e5cd6518e035833af5755f6828f66b42c458e3
Jeff Lai <pjefflai@gmail.com>
2021-11-19 14:58:38 -0800

    FIX: NaN issue

--- src/index.js
+++ src/index.js
@@ -6,25 +6,25 @@ import ExchangeRate from './exchange.js';
 
 function getElement(response, firstCurrency, secondCurrency) {
   if (response.result === "success") {
-    console.log(response.conversion_rate);
-    $("#currencyOutput").text(`${firstCurrency} = ${response.conversion_rate * secondCurrency}`);
+    $("#currencyOutput").text(`$${firstCurrency} USD = ${response.conversion_rate * firstCurrency}${secondCurrency}`);
   } else {
-    $("#error").text(`Sorry, there wasn an error: ${response}`);
+    $("#error").text(`Sorry, there was an error: ${response}`);
   }
 }
 
-async function makeApiCall(firstCurrency, secondCurrency, usdAmount) {
+async function makeApiCall(firstCurrency, secondCurrency) {
   const response = await ExchangeRate.getRate(firstCurrency, secondCurrency);
-  getElement(response, firstCurrency, secondCurrency, usdAmount);
+  getElement(response, firstCurrency, secondCurrency);
 }
 
 $(document).ready(function () {
   $("#exchanger").click(function (e) {
     e.preventDefault();
     console.log("hello");
-    let firstCurrency = parseInt($("#USD").val());
+    let firstCurrency = $("#USD").val();
     console.log(firstCurrency);
     let secondCurrency = $("#foreignCurrency option:selected").val();
+    console.log(secondCurrency);
     makeApiCall(firstCurrency, secondCurrency);
   });
 });
